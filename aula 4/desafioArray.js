e2286bff3dac84c83f49a3881bbd835153b3be9f
PetBarroso <pettersonbarroso@gmail.com>
2022-03-21 21:48:12 -0300

    arrumando exercicio e criando aula4

---
+++ aula 4/desafioArray.js
@@ -0,0 +1,9 @@
+/* 
+    MICRO DESAFIO
+    1. Crie um array que contenha nomes de produtos de mercado para compra;
+    2. Após cria-lo exiba no console o resultado dos métodos relacionados aos arrays.
+    que são: push, pop, shift, unshift, join, indexOf, lastIndexOf e includes.
+    3. Escreva com as suas palavras o que cada método faz.
+    Exemplo: console.log(`Estou transformando tudo em uma string: ${texto}`)
+
+*/
--- exercicio.js
+++ exercicio.js
@@ -1,7 +1,7 @@
 // Em um parque de diversões nos pedem um programa para verificar se os passageiros da montanha-russa
 // podem entrar no brinquedo.
 //
-// Crie uma função controleDeAcesso() que receba dois parâmetros: 
+// Crie uma função controleDeAcesso() que receba dois parâmetros:
 //  -> altura da pessoa;
 //  -> se está acompanhada.
 //
@@ -9,16 +9,18 @@
 //  -> A pessoa deve medir mais de 1.40m e menos de 2m.
 //  -> Se a pessoa medir menos de 1.40m, deverá ir acompanhada.
 //  -> Se a pessoa medir menos de 1.20m, não poderá ir nem acompanhada.
-function controleDeAcesso (altura){
-  if(altura <= 1.20){
-      console.log("não poderá ir nem acompanhada!");
-  } else if (altura > 1.20 && altura < 1.40) {
-      console.log("Deverá ir acompanhado!");
-  } else {
-      console.log("poderá ir!")
+function controleDeAcesso(altura, vemAcompanhada) {
+  if (altura < 1.2) {
+    console.log('não poderá ir nem acompanhada!')
+  } if (altura > 1.2 && altura < 1.4 && vemAcompanhada == true) {
+    console.log('Está acompanhada, pode andar!')
+  } if (altura > 1.20 && altura < 1.40 && vemAcompanhada == false ){
+    console.log('Não pode andar sem acompanhante!')
+  } else if (altura > 1.40 && altura < 2.00) { 
+    console.log ("Pode andar tranquilo!!")
   }
-  }
+}
 
-controleDeAcesso(1.10)
+controleDeAcesso(1.1)
 controleDeAcesso(1.35)
-controleDeAcesso(1.90)
+controleDeAcesso(1.9)
