fb8ada9b88273f30959185c19923c000c70f4f51
PetBarroso <pettersonbarroso@gmail.com>
2022-03-21 21:55:05 -0300

    ajustes

--- exercicio.js
+++ exercicio.js
@@ -12,12 +12,14 @@
 function controleDeAcesso(altura, vemAcompanhada) {
   if (altura < 1.2) {
     console.log('não poderá ir nem acompanhada!')
-  } if (altura > 1.2 && altura < 1.4 && vemAcompanhada == true) {
+  }
+  if (altura > 1.2 && altura < 1.4 && vemAcompanhada == true) {
     console.log('Está acompanhada, pode andar!')
-  } if (altura > 1.20 && altura < 1.40 && vemAcompanhada == false ){
+  }
+  if (altura > 1.2 && altura < 1.4 && vemAcompanhada == false) {
     console.log('Não pode andar sem acompanhante!')
-  } else if (altura > 1.40 && altura < 2.00) { 
-    console.log ("Pode andar tranquilo!!")
+  } else if (altura > 1.4 && altura < 2.0) {
+    console.log('Pode andar tranquilo!!')
   }
 }
 
