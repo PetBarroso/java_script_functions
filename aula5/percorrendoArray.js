36b3d6410ff956c6dccd10bb897393aea737d562
PetBarroso <pettersonbarroso@gmail.com>
2022-03-27 14:51:57 -0300

    percorrendoArray e recalculandoPreco

---
+++ aula5/percorrendoArray.js
@@ -0,0 +1,11 @@
+let lista = ['Salame',
+'Carvão',
+'Carne',
+'Cerveja',
+'refrigerante',
+'queijo',
+'Cerveja']
+
+for(let volta = 0; volta < lista.length; volta++){
+  console.log(lista[volta])
+}
